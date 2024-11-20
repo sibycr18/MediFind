65becb92dcebf5df74e3040f96b98f3ee9972fa5
Siby C.R <sibycr18@gmail.com>
2024-11-20 12:42:02 +0530

    bug fix

--- src/components/MedicineImages.tsx
+++ src/components/MedicineImages.tsx
@@ -5,8 +5,8 @@ interface MedicineImagesProps {
     medicineName: string;
 }
 // Google Search Components
-const API_KEY = import.meta.env.GOOGLE_API_KEY;
-const CX_ID = import.meta.env.GOOGLE_CX_ID;
+const API_KEY = process.env.GOOGLE_API_KEY;
+const CX_ID = process.env.GOOGLE_CX_ID;
 
 const MedicineImages: React.FC<MedicineImagesProps> = ({ medicineName }) => {
     const [images, setImages] = useState<string[]>([]);
